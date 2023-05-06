e53b22cc66d951d76760fad0b262823c88859062
Gitam Gadtaula <gitamgadtaula@Gitams-MacBook-Pro.local>
2023-05-06 17:12:16 +0545

    initial working version of siriusnet swap

---
+++ src/components/CurrencyLogo/index.tsx
@@ -0,0 +1,54 @@
+import { Currency, ETHER, Token } from 'siriusnet-uniswap-sdk'
+import React, { useMemo } from 'react'
+import styled from 'styled-components'
+
+// import EthereumLogo from '../../assets/images/ethereum-logo.png'
+import useHttpLocations from '../../hooks/useHttpLocations'
+import { WrappedTokenInfo } from '../../state/lists/hooks'
+import Logo from '../Logo'
+
+const getTokenLogoURL = (address: string) =>
+  `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+
+const StyledEthereumLogo = styled.img<{ size: string }>`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+  border-radius: 24px;
+`
+
+const StyledLogo = styled(Logo)<{ size: string }>`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+`
+
+export default function CurrencyLogo({
+  currency,
+  size = '24px',
+  style
+}: {
+  currency?: Currency
+  size?: string
+  style?: React.CSSProperties
+}) {
+  const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined)
+
+  const srcs: string[] = useMemo(() => {
+    if (currency === ETHER) return []
+
+    if (currency instanceof Token) {
+      if (currency instanceof WrappedTokenInfo) {
+        return [...uriLocations, getTokenLogoURL(currency.address)]
+      }
+
+      return [getTokenLogoURL(currency.address)]
+    }
+    return []
+  }, [currency, uriLocations])
+
+  if (currency === ETHER) {
+    return <StyledEthereumLogo src={"https://384227940-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FnJE99CMCFrThOniBFkt5%2Fuploads%2FaoU8ToBfBhlnvSWUKu4g%2FMCD%20-%20Copy.png?alt=media&token=5aceb2a1-b61b-41a9-a0ed-6ef159eb5b6e"} size={size} style={style} />
+  }
+
+  return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} />
+}
---
+++ src/constants/lists.ts
@@ -0,0 +1,4 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/warewar1/token-lists/main/test/schema/siriuswap.tokenlist.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
